4aa23a46c3f21387f08fece6536c157cefea27ba
Alexander Fischer <alex.fi93@gmail.com>
2019-07-16 13:51:50 +0200

    add img lazy loading

--- components/pattern/image-and-text.js
+++ components/pattern/image-and-text.js
@@ -1,15 +1,21 @@
 import { object } from "prop-types"
 import { asHtml } from "../../utils/prismic-utils"
 import Parser from "html-react-parser"
+import LazyLoad from "react-lazyload"
 
 const ImageAndText = ({ data }) => {
     const content = asHtml(data.content)
-    
+
+    // Get height or die tryin
+    const height = data.image ? data.image.dimensions.height.toString() + "px" : "0"
+
     return (
         <div className="image-and-text-container">
             <div className="grid align-items-center">
                 <div className="col-4">
-                    <img src={data.image.url} alt={data.image.alt} />
+                    <LazyLoad height={height} offset={200}>
+                        <img src={data.image.url} alt={data.image.alt} />
+                    </LazyLoad>
                 </div>
                 <div className="col-4">
                     <div className="text-container">

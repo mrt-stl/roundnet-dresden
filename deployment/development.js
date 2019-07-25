3d48c7af8ba5d90293d2697a17ce89899da1f708
Alexander Fischer <alex.fi93@gmail.com>
2019-07-25 18:01:59 +0200

    add dynamic deployment for development

---
+++ deployment/development.js
@@ -0,0 +1,82 @@
+const { exec } = require("child_process")
+
+const admin = require("firebase-admin")
+const serviceAccount = require("./firebase-deploy.json")
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+})
+
+const deployCmd = "now --target production -A [FILE] --token huhwXduidTwnFIaZwRiipPoc"
+
+const db = admin.firestore()
+
+const projectsRef = db.collection("projects")
+projectsRef.where("branch", "=", "development").get()
+    .then((snapshot) => {
+        snapshot.forEach((doc) => {
+            console.log(doc.data())
+            deployToNow(doc.data())
+        })
+    })
+    .catch((err) => {
+        console.log("Error getting documents", err)
+    })
+
+function deployToNow(project) {
+    console.log(project)
+    console.log("\n")
+    const cmd = createCmd(project)
+
+    console.log(cmd)
+    exec(cmd, (err, stdout, stderr) => {
+        if (err) {
+            console.error(err)
+            // node couldn't execute the command
+            return
+        }
+
+        // the *entire* stdout and stderr (buffered)
+        console.log(stdout)
+        console.error(stderr)
+    })
+}
+
+function createCmd(project) {
+    const file = "deployment/" + project.project_id + ".json"
+
+    let cmd = deployCmd.replace("[FILE]", file)
+
+    // Add Prismic
+    cmd = cmd + " -b PRISMIC_ENDPOINT=" + stringForCmd(project.prismic_endpoint)
+    cmd = cmd + " -b ACCESS_TOKEN=" + stringForCmd(project.prismic_access_token)
+
+    // Add navigation
+    cmd = cmd + " -b NAV=" + jsonForCmd(project.nav)
+
+    // Add font
+    if (project.font.name !== "") {
+        cmd = cmd + " -b FONT=" + jsonForCmd(project.font)
+    }
+
+    // Show banner or not
+    const showBanner = project.show_banner === "on" ? 1 : 0
+    cmd = cmd + " -b HAS_BANNER=" + stringForCmd(showBanner)
+
+    // Add dark mode setting
+    cmd = cmd + " -b DARK_MODE=" + stringForCmd(project.dark_mode)
+
+    // Add caching time
+    const cachingTime = project.cache_duration !== "" ? project.cache_duration : 15
+    cmd = cmd + " -b CACHING_TIME=" + stringForCmd(cachingTime)
+
+    return cmd
+}
+
+function stringForCmd(str) {
+    return "\"" + str + "\""
+}
+
+function jsonForCmd(str) {
+    return "\"" + JSON.stringify(str).replace(/"/g, "\\\"") + "\""
+}

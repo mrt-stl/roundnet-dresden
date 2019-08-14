0221b30ef13c63b3eb63c7ccfddaee9537e982c7
alexander-fischer <alexanderfischerdev@gmail.com>
2019-08-14 14:48:21 +0200

    add atmospheric model

---
+++ models/atmospheric-model.ts
@@ -0,0 +1,14 @@
+import TukanModel from "./tukan-model"
+import { TukanType } from "./tukan-types"
+
+export default class AtmosphericModel extends TukanModel {
+    imgSrc: string
+    imgAlt: string
+
+    constructor(type: TukanType, imgSrc: string, imgAlt?: string) {
+        super(type)
+
+        this.imgSrc = imgSrc
+        this.imgAlt = imgAlt
+    }
+}

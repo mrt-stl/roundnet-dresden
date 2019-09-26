e19f5bc9183043c65cb070f4a609c58cad9c8a7c
alexander-fischer <alexanderfischerdev@gmail.com>
2019-09-26 10:31:05 +0200

    fix error page

--- pages/_error.js
+++ pages/_error.js
@@ -18,42 +18,21 @@ const Error = () => {
                 data={meta} />
             <Nav />
 
-            <div className="hero-image-container">
-                <img src="../static/img/error.jpg" />
-                <div className="hero-image-content-container text-center">
-                    <h1>Seite nicht verfügbar</h1>
-                    <a href="/" className="link-content">Zurück zur Startseite</a>
+            <div className="error-container">
+                <div className="grid justify-content-center">
+                    <div className="col-6">
+                        <img src="../static/img/error.png" />
+                        <div className="text-center">
+                            <a href="/" className="link-content">Zurück zur Startseite</a>
+                        </div>
+                    </div>
                 </div>
+
             </div>
             <style jsx>{`    
-                    .hero-image-container {
-                        position: relative;
-                        height: 100vh;
-                        width: 100%;
-                    }
-                    .hero-image-content-container {
-                        position: absolute;
-                        top: 50%;
-                        left: 50%;
-                        transform: translate(-50%, -50%); 
-                    }
-                    a {
-                        color: white;
-	                    font-size: 1em;
-	                    letter-spacing: 0.2px;
-                        line-height: 1.5;
-                        margin-bottom: 0.5em;
-                        margin-top: 0.5em;
-                        text-decoration: none;
-                    }
-                    h1 {
-	                    font-size: 4em;
-                        color: white;
-                        letter-spacing: 0.1px;
-                        line-height: 1.25;
-                        font-weight: 400;
-                        margin-bottom: 0.25em;
-                        margin-top: 0.25em;
+                    .error-container {
+                        padding-top: var(--standard-spacing);
+                        padding-bottom: var(--standard-spacing);
                     }
                     img {
                         width: 100%;

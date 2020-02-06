e6261a94f1c569f82d3ba91c4a93350700d3e93a
alexander-fischer <alexfi@pm.me>
2020-02-06 18:01:15 +0100

    add amount to cart link

--- components/shop/cart-link.tsx
+++ components/shop/cart-link.tsx
@@ -4,24 +4,46 @@ import { client } from "../../models/config/shopify"
 
 class CartLink extends Component {
 
+    public state = {
+        total: 0
+    }
+
     public render() {
+        const { total } = this.state
+
         return (
             <div className="cart-link-container align-items-center">
                 <a href="/cart">
-                    Warenkorb
+                    Warenkorb ({total.toFixed(2)} EUR)
                 </a>
             </div>
         )
     }
 
     public async componentDidMount() {
+        let checkout = null
         const savedCheckoutID = getCheckoutID()
         if (!savedCheckoutID) {
-            const checkout = await client.checkout.create()
+            checkout = await client.checkout.create()
 
             const checkoutID = checkout.id
             saveCheckoutID(checkoutID)
+
+        } else {
+            checkout = await client.checkout.fetch(savedCheckoutID)
         }
+
+        const lineItems = checkout.lineItems
+        let total = 0
+        for (const lineItem of lineItems) {
+            const amount = lineItem.quantity
+            const price = lineItem.variant.priceV2.amount
+            const parsedPrice = parseFloat(price)
+
+            total += (amount * parsedPrice)
+        }
+
+        this.setState({ total })
     }
 }
 

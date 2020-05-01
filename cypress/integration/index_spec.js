262d6697c3ee8f4f11f20249d7f6cf64de892ba2
Florian <florian@stadtteilliebe.de>
2020-05-01 17:46:06 +0200

    remove cy "fill in contact form" again

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -14,16 +14,6 @@ describe("Index", () => {
         })
     })
 
-    it("fill in contact form", () => {
-        cy.get(".contact-container").within(() => {
-            cy.get("input[name=fname]").type("Test Example")
-            cy.get("input[name=email]").type("florian@stadtteilliebe.de")
-            cy.get("textarea").type("Stadtteilliebe ist kein Verbrechen")
-
-            cy.get("button").click()
-        })
-    })
-
     it("test cart link", () => {
         cy.get(".cart-link-container").within(() => {
             cy.get("a").click()

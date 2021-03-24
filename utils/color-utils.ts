3e3958d057d058941c6991c66c0e0844be7f09ab
MartinGoesGitHub <kolitsch.martin@gmail.com>
2021-03-24 14:32:07 +0100

    add function to calculate gradients and rgb from hex

--- utils/color-utils.ts
+++ utils/color-utils.ts
@@ -8,11 +8,31 @@ export const isColorLight = (color: string) => {
     // tslint:disable-next-line: no-bitwise
     const b = (rgb >> 0) & 0xff
 
-    const hsp = Math.sqrt(
-        0.299 * (r * r) +
-        0.587 * (g * g) +
-        0.114 * (b * b)
-    )
+    const hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b))
 
     return hsp > 127.5
+}
+
+export const hexToRgb = (color: string) => {
+    // if (color.length !== 6 || color[0] !== "#") {
+    //     throw new Error("Something is wrong with this color string")
+    // }
+    const hexWithoutHash = color.substring(1)
+    const splitHex = hexWithoutHash.match(/.{1,2}/g)
+    const RgbArr = [parseInt(splitHex[0], 16), parseInt(splitHex[1], 16), parseInt(splitHex[2], 16)]
+
+    return RgbArr
+}
+
+export const getGradient = (color: string) => {
+    const RgbArr = hexToRgb(color)
+    const gradientStr = `linear-gradient(45deg, 
+                            rgba(${RgbArr[0]}, ${RgbArr[1]}, ${RgbArr[2]}, 1) 5%,
+                            rgba(${RgbArr[0]}, ${RgbArr[1]}, ${RgbArr[2]}, 0.6) 30%,
+                            rgba(${RgbArr[0]}, ${RgbArr[1]}, ${RgbArr[2]}, 0.2) 60%,
+                            rgba(${RgbArr[0]}, ${RgbArr[1]}, ${RgbArr[2]}, 0.6) 70%,
+                            rgba(${RgbArr[0]}, ${RgbArr[1]}, ${RgbArr[2]}, 1) 95%
+                            )`
+
+    return gradientStr
 }

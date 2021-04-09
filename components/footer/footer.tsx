221f67c1663e8f7a825d119bcf4850d9bc17011c
mrt-stl <martin@stadtteilliebe.de>
2021-04-09 13:01:04 +0200

    prevent failing when no link is given

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -8,80 +8,78 @@ import TukanImage from "../elements/tukan-image"
 import Typewriter from "../elements/typewriter"
 
 interface IFooterProps {
-    data: any
+  data: any
 }
 
 const Footer = (props: IFooterProps) => {
-    if (props.data) {
-        const { footer_content, footer_links, footer_links_social, footer_watermark } = props.data
-        return (
-            <footer>
-                <FooterContainer>
-                    <FooterGrid valign="top" halign="center">
-                        <>
-                            <TCol size={1 / 4} collapse="md" talign="left">
-                                {parse(asHtml(footer_content))}
-                            </TCol>
+  if (props.data) {
+    const { footer_content, footer_links, footer_links_social, footer_watermark } = props.data
+    return (
+      <footer>
+        <FooterContainer>
+          <FooterGrid valign="top" halign="center">
+            <>
+              <TCol size={1 / 4} collapse="md" talign="left">
+                {parse(asHtml(footer_content))}
+              </TCol>
 
-                            <TCol size={1 / 4} collapse="md" talign="left">
-                                {footer_links.map((element, index) => {
-                                    return (
-                                        <div key={index}>
-                                            <a href={linkResolver(element.footer_link)}>
-                                                <p>{element.footer_label}</p>
-                                            </a>
-                                        </div>
-                                    )
-                                })}
-                            </TCol>
+              <TCol size={1 / 4} collapse="md" talign="left">
+                {footer_links.map((element, index) => {
+                  return (
+                    <div key={index}>
+                      <a href={linkResolver(element.footer_link)}>
+                        <p>{element.footer_label}</p>
+                      </a>
+                    </div>
+                  )
+                })}
+              </TCol>
 
-                            <TCol size={2 / 4} collapse="md" talign="right" salign="center">
-                                <SocialMediaContainer>
-                                    {footer_links_social.map((element, index) => {
-                                        return (
-                                            <a href={linkResolver(element.social_link)} key={index} target="_blank">
-                                                <TukanImage
-                                                    src={element.footer_image.url}
-                                                    alt={element.footer_image.alt}
-                                                    height="auto"
-                                                    width="auto"
-                                                />
-                                            </a>
-                                        )
-                                    })}
-                                </SocialMediaContainer>
-                            </TCol>
-                            <TCol size={1}>
-                                <Typewriter strArr={["Gemacht mit Stadtteilliebe", `${footer_watermark}`]} />
-                            </TCol>
-                        </>
-                    </FooterGrid>
-                </FooterContainer>
-            </footer>
-        )
-    }
-    return null
+              <TCol size={2 / 4} collapse="md" talign="right" salign="center">
+                <SocialMediaContainer>
+                  {footer_links_social.map((element, index) => {
+                    if (element.footer_image.url) {
+                      return (
+                        <a href={linkResolver(element.social_link)} key={index} target="_blank">
+                          <TukanImage src={element.footer_image.url} alt={element.footer_image.alt} height="auto" width="auto" />
+                        </a>
+                      )
+                    }
+                    return null
+                  })}
+                </SocialMediaContainer>
+              </TCol>
+              <TCol size={1}>
+                <Typewriter strArr={["Gemacht mit Stadtteilliebe", `${footer_watermark}`]} />
+              </TCol>
+            </>
+          </FooterGrid>
+        </FooterContainer>
+      </footer>
+    )
+  }
+  return null
 }
 
 const FooterContainer = styled.div`
-    padding-bottom: ${(props) => props.theme.spacing.xl};
-    padding-top: ${(props) => props.theme.spacing.xl};
-    background-color: ${(props) => props.theme.projectColors.secondary};
-    height: auto;
-    font-size: 14px;
+  padding-bottom: ${(props) => props.theme.spacing.xl};
+  padding-top: ${(props) => props.theme.spacing.xl};
+  background-color: ${(props) => props.theme.projectColors.secondary};
+  height: auto;
+  font-size: 14px;
+  color: ${(props) => props.theme.projectColors.onSecondary};
+
+  a {
     color: ${(props) => props.theme.projectColors.onSecondary};
+    transition: all 0.1s ease-in-out;
+  }
 
-    a {
-        color: ${(props) => props.theme.projectColors.onSecondary};
-        transition: all 0.1s ease-in-out;
-    }
+  a:hover {
+    opacity: 0.7;
+    transition: all 0.1s ease-in-out;
+  }
 
-    a:hover {
-        opacity: 0.7;
-        transition: all 0.1s ease-in-out;
-    }
-
-    ${media.maxWidth("md")`
+  ${media.maxWidth("md")`
         a {
             color: ${(props) => props.theme.projectColors.onSecondary};
         }
@@ -96,33 +94,33 @@ const FooterContainer = styled.div`
 `
 
 const FooterGrid = styled(TGrid)`
-    height: 100%;
+  height: 100%;
 
-    ${media.maxWidth("md")`
+  ${media.maxWidth("md")`
         margin-left: 40px;
         margin-right: 40px;
     `}
 `
 
 const SocialMediaContainer = styled.div`
-    align-self: center;
+  align-self: center;
 
-    a:not(:first-child) {
-        margin-left: ${(props) => props.theme.spacing.s};
-        transition: all 0.1s ease-in-out;
-    }
+  a:not(:first-child) {
+    margin-left: ${(props) => props.theme.spacing.s};
+    transition: all 0.1s ease-in-out;
+  }
 
-    a:hover {
-        opacity: 0.7;
-        transition: all 0.1s ease-in-out;
-    }
+  a:hover {
+    opacity: 0.7;
+    transition: all 0.1s ease-in-out;
+  }
 
-    img {
-        height: 32px;
-        width: 32px;
-    }
+  img {
+    height: 32px;
+    width: 32px;
+  }
 
-    ${media.maxWidth("md")`
+  ${media.maxWidth("md")`
         a:not(:first-child) {
             margin-left: calc( 3 * ${(props) => props.theme.spacing.xxs});
             transition: all 0.1s ease-in-out;

79149fb18248ee4d4c447edf392f941f010a5c5e
Florian Swoboda <florian@stadtteilliebe.de>
2021-04-11 21:25:39 +0200

    fix height for location-pattern on mobile

--- components/elements/marker.tsx
+++ components/elements/marker.tsx
@@ -1,3 +1,5 @@
+import styled, { keyframes } from "styled-components"
+
 interface IMarkerProps {
     lat: number
     lng: number
@@ -7,24 +9,60 @@ interface IMarkerProps {
 const Marker = (_props: IMarkerProps) => {
 
     return (
-        <div className="marker-container">
+        <MarkerContainer>
+            <div className="shadow" />
             <img src="/static/icon/marker.svg" />
-            <style jsx>{`
-                .marker-container {
-                    position: absolute;
-                    top: -28px;
-                    left: -16px;
-                    width: 32px;
-                    height: 32px;
-                }
-
-                img {
-                    width: 100%;
-                    height: 100%;
-                }
-            `}</style>
-        </div>
+        </MarkerContainer>
     )
 }
+
+const pulse = keyframes`
+    0% {
+        transform: scale(1);
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    40% {
+        transform: scale(0.7);
+        background-color: rgba(0, 0, 0, 0.2);
+    }
+
+    100% {
+        transform: scale(1);
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+`
+
+const jump = keyframes`
+    0% {
+        top: -40px;
+    }
+
+    40% {
+        top: -52px;
+    }
+
+    100% {
+        top: -40px;
+    }
+`
+
+const MarkerContainer = styled.div`
+    .shadow {
+        animation: ${pulse} 3s infinite;
+        border-radius: 50%;
+        width: 24px;
+        height: 12px;
+    }
+
+    img {
+        animation: ${jump} 3s infinite;
+        width: auto;
+        height: 48px;
+
+        position: absolute;
+        left: -12px;
+    }
+`
 
 export default Marker
--- components/pattern/location.tsx
+++ components/pattern/location.tsx
@@ -1,53 +1,54 @@
+import { media } from "../style/tukan"
 import GoogleMapReact from "google-map-react"
 import Marker from "../elements/marker"
-import LazyLoad from "react-lazyload"
+import styled from "styled-components"
 
 export interface ILocationProps {
-  items: any
+    items: any
 }
 
 const Location = (props: ILocationProps) => {
-  const items = props.items
+    const items = props.items
+    
+    const locationCount = items.length
+    const zoom = locationCount === 1 ? 15 : 9.5
+    let sumLat: number = 0
+    let sumLng: number = 0
 
-  const locationCount = items.length
-  const zoom = locationCount === 1 ? 15 : 9.5
-  let sumLat: number = 0
-  let sumLng: number = 0
-
-  items.map((item) => {
+    items.map((item) => {
     sumLat += item.location_coords.latitude
     sumLng += item.location_coords.longitude
-  })
+})
 
-  const lat = sumLat / locationCount
-  const lng = sumLng / locationCount
+    const lat = sumLat / locationCount
+    const lng = sumLng / locationCount
 
-  const center = {
-    lat,
-    lng,
-  }
+    const center = {
+        lat,
+        lng,
+    }
+    
+    return (
+        <LocationContainer>
+            <GoogleMapReact bootstrapURLKeys={{ key: "AIzaSyBg2SZDYXK6Zv_UXZdVJJr5J8eRlSAVUKQ" }} defaultCenter={center} defaultZoom={zoom}>
+                {items.map((item, index) => {
+                    const lat = item.location_coords.latitude
+                    const lng = item.location_coords.longitude
 
-  return (
-    <LazyLoad height="512px" offset={300}>
-      <div className="location-container">
-        <GoogleMapReact bootstrapURLKeys={{ key: "AIzaSyBg2SZDYXK6Zv_UXZdVJJr5J8eRlSAVUKQ" }} defaultCenter={center} defaultZoom={zoom}>
-          {items.map((item, index) => {
-            const lat = item.location_coords.latitude
-            const lng = item.location_coords.longitude
-
-            return <Marker lat={lat} lng={lng} key={index} />
-          })}
-        </GoogleMapReact>
-
-        <style jsx>{`
-          .location-container {
-            height: 512px;
-            width: 100%;
-          }
-        `}</style>
-      </div>
-    </LazyLoad>
+                    return <Marker lat={lat} lng={lng} key={index} />
+                })}
+            </GoogleMapReact>
+        </LocationContainer>
   )
 }
+
+const LocationContainer = styled.div`
+    height: 480px;
+    width: 100%;
+
+    ${media.maxWidth("md")`
+        height: 240px;
+    `}
+`
 
 export default Location

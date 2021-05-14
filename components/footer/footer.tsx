742f3a84a8fd16f28770b9109d23a1d8e8d7e009
mrt-stl <martin@stadtteilliebe.de>
2021-05-14 16:51:43 +0200

    customize footer and add partners logos

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,97 +4,98 @@ import parse from "html-react-parser"
 import { asHtml } from "../../utils/prismic-utils"
 import { TGrid, TCol } from "../style/sc-grid"
 import styled from "styled-components"
-import TukanImage from "../elements/tukan-image"
-import Typewriter from "../elements/typewriter"
-
 interface IFooterProps {
-  data: any
+    data: any
 }
 
 const Footer = (props: IFooterProps) => {
-  if (props.data) {
-    const { footer_content_col_1, footer_content_col_2, footer_links, footer_links_social, footer_watermark } = props.data
-    return (
-      <footer>
-        <FooterContainer>
-          <FooterGrid valign="top" halign="center">
+    if (props.data) {
+        const { footer_partners, footer_content_left, footer_content_right, footer_links, footer_links_bottom } = props.data
+        return (
             <>
-              <TCol size={1 / 3} collapse="md" talign="left">
-                {parse(asHtml(footer_content_col_1))}
-              </TCol>
+                <PartnersContainer>
+                    <TGrid valign="center">
+                        {footer_partners.map((element, index) => {
+                            return <TCol key={index}>{parse(asHtml(element.content))}</TCol>
+                        })}
+                    </TGrid>
+                </PartnersContainer>
+                <footer>
+                    <FooterContainer>
+                        <FooterGrid valign="top" halign="center">
+                            <TCol size={1 / 3} collapse="md" talign="left">
+                                {parse(asHtml(footer_content_left))}
+                            </TCol>
 
-              <TCol size={1 / 3} collapse="md" talign="left">
-                {footer_content_col_2 ? parse(asHtml(footer_content_col_2)) : null}
-                {footer_links.map((element, index) => {
-                  return (
-                    <div key={index} style={{ marginTop: "0.5em" }}>
-                      <a href={linkResolver(element.footer_link)}>{element.footer_label}</a>
-                    </div>
-                  )
-                })}
-              </TCol>
+                            <LinkList size={1 / 3} collapse="md" talign="left">
+                                {footer_links.map((element, index) => {
+                                    return (
+                                        <a href={linkResolver(element.footer_link)} key={index}>
+                                            {element.footer_label}
+                                        </a>
+                                    )
+                                })}
+                            </LinkList>
 
-              <TCol size={1 / 3} collapse="md" talign="right" salign="center">
-                <SocialMediaContainer>
-                  {footer_links_social.map((element, index) => {
-                    if (element.footer_image.url) {
-                      return (
-                        <a href={linkResolver(element.social_link)} key={index} target="_blank">
-                          <TukanImage src={element.footer_image.url} alt={element.footer_image.alt} height="auto" width="auto" />
-                        </a>
-                      )
-                    }
-                    return null
-                  })}
-                </SocialMediaContainer>
-              </TCol>
-              <TCol size={1}>
-                <Typewriter strArr={["Gemacht mit Stadtteilliebe", `${footer_watermark}`]} />
-              </TCol>
+                            <TCol size={1 / 3} collapse="md" talign="left">
+                                {parse(asHtml(footer_content_right))}
+                            </TCol>
+
+                            <LinkListBottom size={1}>
+                                {footer_links_bottom.map((element, index) => {
+                                    return (
+                                        <a href={linkResolver(element.link)} key={index}>
+                                            {element.label}
+                                        </a>
+                                    )
+                                })}
+                            </LinkListBottom>
+                        </FooterGrid>
+                    </FooterContainer>
+                </footer>
             </>
-          </FooterGrid>
-        </FooterContainer>
-      </footer>
-    )
-  }
-  return null
+        )
+    }
+    return null
 }
 
+const PartnersContainer = styled.div`
+    background-color: ${(props) => props.theme.projectColors.grey30};
+    padding-top: ${(props) => props.theme.spacing.m};
+    padding-bottom: ${(props) => props.theme.spacing.s};
+
+    img {
+        filter: grayscale(100%);
+    }
+`
+
 const FooterContainer = styled.div`
-  padding-bottom: ${(props) => props.theme.spacing.xl};
-  padding-top: ${(props) => props.theme.spacing.xl};
-  background-color: ${(props) => props.theme.projectColors.green};
-  height: auto;
-  font-size: ${(props) => props.theme.fontSize.xs};
-  color: ${(props) => props.theme.projectColors.yellow};
+    padding-top: ${(props) => props.theme.spacing.xl};
+    background-color: ${(props) => props.theme.projectColors.grey90};
+    height: auto;
+    font-size: ${(props) => props.theme.fontSize.xs};
+    color: white;
 
-  a {
-    color: ${(props) => props.theme.projectColors.yellow};
-    transition: 0.15s ease-in-out;
-  }
+    a {
+        transition: 0.15s ease-in-out;
+        color: white;
+    }
 
-  a:hover {
-    opacity: 0.6;
-    transition: 0.15s ease-in-out;
-  }
+    a:hover {
+        opacity: 0.6;
+        transition: 0.15s ease-in-out;
+    }
 
-  img {
-    max-height: 120px;
-  }
+    img {
+        max-height: 120px;
+        margin-bottom: ${(props) => props.theme.spacing.m};
+    }
 
-  span.contact {
-      width: 20px;
-      display: inline-block;
-  }
-
-  ${media.maxWidth("md")`
-        a {
-            color: ${(props) => props.theme.projectColors.yellow};
-        }
+    ${media.maxWidth("md")`
 
         * {
             text-align: left !important;
-            color: ${(props) => props.theme.projectColors.yellow};
+            color: white;
         }
 
         /* margin: ${(props) => props.theme.spacing.m} ${(props) => props.theme.spacing.s}; */
@@ -102,48 +103,26 @@ const FooterContainer = styled.div`
 `
 
 const FooterGrid = styled(TGrid)`
-  height: 100%;
+    height: 100%;
 
-  ${media.maxWidth("md")`
+    ${media.maxWidth("md")`
         margin-left: 40px;
         margin-right: 40px;
     `}
 `
 
-const SocialMediaContainer = styled.div`
-  align-self: center;
+const LinkList = styled(TCol)`
+    a {
+        display: block;
+        margin-bottom: ${(props) => props.theme.spacing.m};
+    }
+`
 
-  a:not(:first-child) {
-    margin-left: ${(props) => props.theme.spacing.s};
-    transition: 0.15s ease-in-out;
-  }
-
-  a:hover {
-    opacity: 0.6;
-    transition: 0.15s ease-in-out;
-  }
-
-  img {
-    height: 32px;
-    width: 32px;
-  }
-
-  ${media.maxWidth("md")`
-        a:not(:first-child) {
-            margin-left: calc( 3 * ${(props) => props.theme.spacing.xxs});
-            transition: all 0.1s ease-in-out;
-        }
-
-        a:hover {
-            opacity: 1;
-            transition: all 0.1s ease-in-out;
-        }
-
-        img {
-            height: 28px;
-            width: 28px;
-        }
-    `};
+const LinkListBottom = styled(TCol)`
+    a {
+        color: ${(props) => props.theme.projectColors.grey40};
+        margin-right: ${(props) => props.theme.spacing.m};
+    }
 `
 
 export default Footer

4a1de9d9f6a5edd1d00c5d62e5c0f809b03bc291
mrt-stl <martin@stadtteilliebe.de>
2022-01-21 17:05:06 +0100

    add sending confirmation message to user

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -24,12 +24,26 @@ export default async function (req, res) {
     const mailData = {
         from: "tf-ventures-mailing@stlb.me",
         to: "martin@stadtteilliebe.de",
+        replyTo: req.body.email,
         subject: `Message From ${req.body.name}`,
         text: req.body.content + " | Sent from: " + req.body.email,
         html: `<div>${req.body.content}</div><p>Sent from:
       ${req.body.email}</p>`,
     }
 
+    const mailDataConfirmation = {
+        from: "tf-ventures-mailing@stlb.me",
+        to: req.body.email,
+        subject: `Your message to TF Ventures`,
+        text: "Your message: " + req.body.content + " | Sent from: " + req.body.email,
+        html: `<p>Your message: </p><div>${req.body.content}</div><p>Sent from:
+      ${req.body.email}</p><p>Thank you fore your message, we will reach out to you as soon as possible</p><p>Your TF Ventures Team</p>`,
+    }
+
+    // send confirmation email to user
+    transporter.sendMail(mailDataConfirmation)
+
+    // send actual mail to TF Ventures
     const mailRes = await transporter.sendMail(mailData)
     if (!mailRes.messageId) {
         console.error(mailRes)

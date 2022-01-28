9ac528881dabf2128e3b435e7304939fb46adfbe
mrt-stl <martin@stadtteilliebe.de>
2022-01-28 14:43:17 +0100

    add favicons

--- components/meta.tsx
+++ components/meta.tsx
@@ -7,10 +7,9 @@ interface IMetaProps {
 }
 
 const Meta = (props: IMetaProps) => {
-    // Icon urls
     const metaTitle = !isUndefinedOrNullOrEmpty(props.data.metaTitle)
         ? props.data.metaTitle
-        : process.env.PROJECT_NAME
+        : "TF Ventures"
     const metaDescription = !isUndefinedOrNullOrEmpty(props.data.metaDescription)
         ? props.data.metaDescription
         : "Ihr Pflegedienst im Dresdner Zentrum"
@@ -19,9 +18,14 @@ const Meta = (props: IMetaProps) => {
         <div>
             <Head>
                 <meta charSet="utf-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-                <title>{metaTitle ?? ""}</title>
+                <title>{metaTitle}</title>
+                
+                <link rel="icon" href="/favicon.ico" sizes="any"></link>
+                <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <link rel="manifest" href="/site.webmanifest" />
 
                 <meta name="author" content={props.data.metaAuthor} />
                 <meta name="description" content={metaDescription} />

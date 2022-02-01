4351e31e72cd3793c2a86e2a1e1965ac7c8572a9
mrt-stl <martin@stadtteilliebe.de>
2022-02-01 16:25:03 +0100

    improve verification handling and remove confirmation message for user of contact form

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -27,27 +27,17 @@ export default async function (req, res) {
         replyTo: req.body.email,
         subject: `Message From ${req.body.name}`,
         text: req.body.content + " | Sent from: " + req.body.email,
-        html: `<div>${req.body.content}</div><p>Sent from:
+        html: `<p>Name: ${req.body.email}</p><div>Message: ${req.body.content}</div><p>Reply to:
       ${req.body.email}</p>`,
     }
 
-    const mailDataConfirmation = {
-        from: "tf-ventures-mailing@stlb.me",
-        to: req.body.email,
-        subject: `Your message to TF Ventures`,
-        text: "Your message: " + req.body.content + " | Sent from: " + req.body.email,
-        html: `<p>Your message: </p><div>${req.body.content}</div><p>Sent from:
-      ${req.body.email}</p><p>Thank you fore your message, we will reach out to you as soon as possible</p><p>Your TF Ventures Team</p>`,
-    }
+    try {
+        // send actual mail to TF Ventures
+        await transporter.sendMail(mailData)
 
-    // send confirmation email to user
-    transporter.sendMail(mailDataConfirmation)
-
-    // send actual mail to TF Ventures
-    const mailRes = await transporter.sendMail(mailData)
-    if (!mailRes.messageId) {
-        console.error(mailRes)
+        res.status(200).end()
+    } catch (err) {
+        console.log(err)
         res.status(500).end()
     }
-    res.status(200).end()
 }
--- pages/api/verify.ts
+++ pages/api/verify.ts
@@ -4,14 +4,13 @@ export default async function (req, res) {
     const secret = process.env.HCAPTCHA_SECRET
     const token = req.body
 
-    verify(secret, token)
-        .then((data) => {
-            if (data.success === true) {
-                res.status(200).end()
-            } else {
-                console.log("verification failed")
-                res.status(403).end()
-            }
-        })
-        .catch(console.error)
+    const data = await verify(secret, token)
+
+    if (data.success) {
+        res.status(200).end()
+    } else {
+        console.log("verification failed")
+        console.error(data)
+        res.status(403)
+    }
 }

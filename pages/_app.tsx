b0fba7c2b8918d7ba551730a00a671335893c828
mrt-stl <martin@stadtteilliebe.de>
2022-05-21 17:48:31 +0200

    add panelbear

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from "styled-components"
 import { GlobalStyles } from "../components/style/tukan"
 import { theme } from "../components/style/tukan"
+import { usePanelbear } from "@panelbear/panelbear-nextjs"
 
 export default function App({ Component, pageProps }) {
+    usePanelbear("JjsYCbNtceq")
+
     return (
         <>
             <ThemeProvider theme={theme}>
-            <GlobalStyles />
+                <GlobalStyles />
                 <Component {...pageProps} />
             </ThemeProvider>
         </>

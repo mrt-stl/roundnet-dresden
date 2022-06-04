674b2ddb968e8d04db77075537aa8b25e92dae45
mrt-stl <martin@stadtteilliebe.de>
2022-06-04 11:59:17 +0200

    improve footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,49 +1,22 @@
 import { linkResolver } from "../../utils/prismic-utils"
 import Image from "next/image"
 import Link from "next/link"
-import parse from "html-react-parser"
-import { asHtml } from "../../utils/prismic-utils"
 import { TGrid, TCol } from "../style/sc-grid"
-import { FooterContainer, Headline, News, Content, FooterMainContentCol } from "./styles"
+import { FooterContainer, Headline, Content, LegalsCol } from "./styles"
 interface IFooterProps {
     data: any
 }
 
 const Footer = (props: IFooterProps) => {
     if (props.data) {
-        const {
-            contact_headline,
-            contact_content,
-            social_headline,
-            social_links,
-            copyright,
-            footer_links,
-            news_headline,
-            news,
-        } = props.data
+        const { social_headline, social_links, copyright, footer_links } = props.data
         return (
             <>
                 <FooterContainer>
                     <TGrid style={{ height: "100%" }}>
-                        {/* START NEWS SECTION */}
-                        <TCol size={1 / 2} collapse="lg">
-                            <Headline>{news_headline}</Headline>
-                            {news.map((article, index) => (
-                                <News key={index}>{parse(asHtml(article.news_content))}</News>
-                            ))}
-                        </TCol>
-                        {/* END NEW SECTION */}
-
-                        {/* START FOOTER MAIN CONTENT */}
-                        <FooterMainContentCol size={1 / 2} collapse="lg">
-                            {/* CONTACT */}
-                            <TCol size={1 / 2}>
-                                <Headline accent={true}>{contact_headline}</Headline>
-                                <Content>{parse(asHtml(contact_content))}</Content>
-                            </TCol>
-
-                            {/* SOCIAL MEDIA */}
-                            <TCol size={1 / 2}>
+                        {/* SOCIAL MEDIA */}
+                        {social_headline || social_links.length > 0 ? (
+                            <TCol size={1}>
                                 <Headline accent={true}>{social_headline}</Headline>
                                 {social_links
                                     ? social_links.map((link, index) => (
@@ -64,22 +37,22 @@ const Footer = (props: IFooterProps) => {
                                       ))
                                     : null}
                             </TCol>
+                        ) : null}
 
-                            {/* COPYRIGHT & LINKS */}
-                            <TCol size={1} style={{ padding: 0, display: "flex" }}>
-                                <Content style={{ width: "50%", paddingLeft: "40px" }}>
-                                    <p style={{ whiteSpace: "nowrap" }}>{copyright}</p>
-                                </Content>
+                        {/* COPYRIGHT & LINKS */}
+                        <LegalsCol size={1}>
+                            <Content>
+                                <p style={{ whiteSpace: "nowrap" }}>{copyright}</p>
+                            </Content>
 
-                                <Content style={{ width: "50%", paddingLeft: "40px" }}>
-                                    {footer_links.map((link, index) => (
-                                        <Link href={linkResolver(link.footer_link)} key={index}>
-                                            {link.footer_link_label}
-                                        </Link>
-                                    ))}
-                                </Content>
-                            </TCol>
-                        </FooterMainContentCol>
+                            <Content>
+                                {footer_links.map((link, index) => (
+                                    <Link href={linkResolver(link.footer_link)} key={index}>
+                                        {link.footer_link_label}
+                                    </Link>
+                                ))}
+                            </Content>
+                        </LegalsCol>
                     </TGrid>
                 </FooterContainer>
             </>
--- components/footer/styles.tsx
+++ components/footer/styles.tsx
@@ -3,10 +3,11 @@ import { TCol } from "../style/sc-grid"
 import { media } from "../style/tukan"
 
 export const FooterContainer = styled.footer`
-    height: 440px;
     position: relative;
-    padding-top: ${(props) => props.theme.spacing.xl};
-    padding-bottom: ${(props) => props.theme.spacing.m};
+    margin-top: ${(props) => props.theme.spacing.xl};
+    padding-top: ${(props) => props.theme.spacing.s};
+    padding-bottom: ${(props) => props.theme.spacing.s};
+    background-color: ${(props) => props.theme.color.blackCoral};
 
     a {
         transition: all 0.2s linear;
@@ -17,25 +18,10 @@ export const FooterContainer = styled.footer`
         color: ${(props) => props.theme.color.bitterlemon};
     }
 
-    :after {
-        content: "";
-        display: block;
-        width: 50vw;
-        background-color: ${(props) => props.theme.color.blackCoral};
-        position: absolute;
-        top: 0;
-        right: 0;
-        height: 100%;
-        z-index: -1;
-    }
-
     ${media.maxWidth("lg")`
         height: fit-content;
         padding-top: ${(props) => props.theme.spacing.s};
         padding-bottom:0;
-        :after {
-            display: none;
-        }
     `}
 `
 
@@ -43,44 +29,6 @@ export const Headline = styled.p<{ accent?: boolean }>`
     color: ${(props) => (props.accent ? props.theme.color.bitterlemon : null)};
     text-transform: uppercase;
     letter-spacing: 2px;
-`
-
-export const News = styled.div`
-    border-left: 2px solid ${(props) => props.theme.color.bitterlemon};
-    margin-top: ${(props) => props.theme.spacing.s};
-    padding-left: ${(props) => props.theme.spacing.s};
-    font-size: ${(props) => props.theme.fontSize.s};
-    line-height: ${(props) => props.theme.lineHeight.l};
-
-    ${media.minWidth("lg")`
-    padding-right: ${(props) => props.theme.spacing.s};
-    `}
-`
-
-export const FooterMainContentCol = styled(TCol)`
-    position: relative;
-    display: flex;
-    flex-wrap: wrap;
-    align-content: space-between;
-
-    ${media.maxWidth("lg")`
-        height: fit-content;
-        padding-left: 0;
-        margin-top: ${(props) => props.theme.spacing.m};
-        padding-top: ${(props) => props.theme.spacing.m};
-        padding-bottom: ${(props) => props.theme.spacing.m};
-        :after {
-            content: "";
-            display: block;
-            width: 100%;
-            height: 100%;
-            background-color: ${(props) => props.theme.color.blackCoral};
-            position: absolute;
-            top: 0;
-            right: 0;
-            z-index: -1;
-        }
-    `}
 `
 
 export const Content = styled.div<{ social?: boolean }>`
@@ -101,4 +49,13 @@ export const Content = styled.div<{ social?: boolean }>`
             margin-left: ${(props) => props.theme.spacing.xs};
         }
     }
+`
+export const LegalsCol = styled(TCol)`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    ${media.maxWidth("lg")`
+        flex-direction: column-reverse;
+    `}
 `
